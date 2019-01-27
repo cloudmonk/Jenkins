7d4cac2bf6fb5d98d464720f3e2958ebbf62db21
neghtedari <neghtedari@loyalty.com>
2019-01-27 00:30:41 -0500

    lkajhsdf

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-<<<<<<< HEAD
-echo "Tlaksjhfklajsdfasfd"
-=======
-echo "fuck this shit"
->>>>>>> dc9e0b3016ab6cf9a4b5db7958a2f99f19c1f694
+echo "alsdkhfda"
+echo "sex "
+
